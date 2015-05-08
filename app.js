5b789a46e2b62e37fce11cb3587c25aaad604b8d
Jacob Marshall <git@jacobmarshall.co>
2015-05-09 11:42:25 +1200

    Copy ghost content folder (if none exists)

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-var path = require('path'),
+var fs = require('fs-extra'),
+    path = require('path'),
     ghost = require('ghost'),
     contentPath = path.join(__dirname, 'content'),
+    ghostContentPath = path.join(__dirname, 'node_modules/ghost/content'),
     configFile = path.join(__dirname, 'config.js');
 
-var app = ghost({
-    config: configFile,
-    paths: {
-        contentPath: contentPath
+fs.stat(contentPath, function (err) {
+    if (err) {
+        fs.copy(ghostContentPath, contentPath, function (err) {
+            if (err) throw new Error('Failed to copy ghost content folder. ' + err);
+            start();
+        });
+    } else {
+        start();
     }
 });
 
-app.then(function (server) {
-    server.start();
-});
+function start () {
+    ghost({
+        config: configFile,
+        paths: {
+            contentPath: contentPath
+        }
+    }).then(function (server) {
+        server.start();
+    });
+}
