2246268c9b7369b6151e5657b1a5233675fee1ab
Jacob Marshall <git@jacobmarshall.co>
2015-05-09 12:32:42 +1200

    Fix issue with dokku volume

--- app.js
+++ app.js
@@ -2,24 +2,22 @@ var fs = require('fs-extra'),
     path = require('path'),
     ghost = require('ghost'),
     contentPath = path.join(__dirname, 'content'),
+    contentFolders = ['apps', 'data', 'images', 'themes'],
     ghostContentPath = path.join(__dirname, 'node_modules/ghost/content'),
     configFile = path.join(__dirname, 'config.js');
 
-fs.stat(contentPath, function (err) {
-    if (err) {
-        fs.readdir(contentPath, function (err, files) {
-            if (err) throw new Error('Unable to read from content folder.');
-            if (files.length) return start();
-
-            fs.copy(ghostContentPath, contentPath, function (err) {
-                if (err) throw new Error('Failed to copy ghost content folder. ' + err);
-                start();
-            });
-        });
-    } else {
+function copy () {
+    fs.copy(ghostContentPath, contentPath, function (err) {
+        if (err) throw new Error('Failed to copy ghost content folder. ' + err);
         start();
-    }
-});
+    });
+}
+
+function contains (arr, items) {
+    return items.every(function (item) {
+        return arr.indexOf(item) !== -1;
+    });
+}
 
 function start () {
     ghost({
@@ -31,3 +29,14 @@ function start () {
         server.start();
     });
 }
+
+fs.stat(contentPath, function (err) {
+    if (err) {
+        copy();
+    } else {
+        fs.readdir(contentPath, function (err, files) {
+            if (files && contains(files, contentFolders)) return start();
+            copy();
+        });
+    }
+});
